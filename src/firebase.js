aebdca4234ad9436b1d2355dc8682243493d7888
Emikgost <Emikgost@gmail.com>
2026-07-06 12:12:14 -0500

    Integrate Firebase Auth and Firestore
    Added Firebase authentication and Firestore imports and exports.

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,8 +10,10 @@ const firebaseConfig = {
   projectId: "progress-635f7",
   storageBucket: "progress-635f7.firebasestorage.app",
   messagingSenderId: "15441470395",
-  appId: "1:15441470395:web:169a99e7844fff89801a9e"
+  appId: "1:15441470395:web:169a99e7844fff89801a9e",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
