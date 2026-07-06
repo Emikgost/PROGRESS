668e810b0b96bcea0484e001043e4a806aa46104
Emikgost <Emikgost@gmail.com>
2026-07-06 12:46:02 -0500

    Wrap Dashboard component with AuthGate

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Dashboard from './Dashboard.jsx'
+import AuthGate from './AuthGate.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Dashboard />
+    <AuthGate>
+      <Dashboard />
+    </AuthGate>
   </React.StrictMode>
 )
